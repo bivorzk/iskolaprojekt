62c95f00e91978c89e908a18708746f96017dec9
bivorzk <bzkugli@gmail.com>
2025-09-05 12:14:13 +0200

    Update passwordhash.js

--- passwordhash.js
+++ passwordhash.js
@@ -1,27 +1,63 @@
 const bcrypt = require('bcrypt');
+const http = require('node:http');
+const querystring = require('querystring');
+
 
 const salt = 10;
+const userPassword = 'asd';
 
-const userPassword = 'user_password';
-bcrypt.hash(userPassword, salt, (err, hash) => {
-    if (err) {
-        return;
-    }
 
-console.log('Hashed password:', hash);
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/login') {
+    let body = '';
+    
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    
+    req.on('end', () => {
+      const parsedData = querystring.parse(body);
+      const username = parsedData.username;
+      const password = parsedData.password;
 
+      console.log('Username:', username);
+      console.log('Password:', password);
+
+      // Hash the received password
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error hashing password');
+        }
+
+        console.log('Hashed password:', hash);
+
+        // Compare with stored password (for demo, using userPassword)
+        bcrypt.compare(userPassword, hash, (err, result) => {
+          if (err) {
+            res.writeHead(500);
+            return res.end('Error comparing password');
+          }
+
+          if (result) {
+            console.log('mukodik');
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(`Welcome, ${username}. Hash: ${hash}`);
+          } else {
+            console.log('nem mukodik');
+            res.writeHead(401);
+            res.end('Authentication failed');
+          }
+        });
+      });
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
 });
-hash = '$2b$10$pnrafxAO5SwEFy4xuWZWquINrQEd70s6XIxI.oxXIPxfrKw3vtI9a'
 
-bcrypt.compare(userPassword,hash, (err, result) => {
-    if (err) {
-        return;
-    }
 
-if (result){
-    console.log('mukodik');
-}else{
-    console.log('nem mukodik')
-}
-
-})
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
