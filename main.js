5295359928d8e3553cb4d594991b2d28b4024c6a
bivorzk <bzkugli@gmail.com>
2025-10-02 15:07:38 +0200

    Refactor code structure and remove redundant changes

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const rateLimit = require('express-rate-limit');
 const passwordhash = require('./passwordhash');
 const database = require('./database');
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use('/database', limiter);
+
+
 
 // Mount routes
 app.use('/passwordhash', passwordhash);
