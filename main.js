29cde12ca6a90f6a8f02442755f47ac890324cd4
bivorzk <bzkugli@gmail.com>
2025-10-07 08:23:47 +0200

    changed limiter to an hour instead of 15 minutes

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes)
+  windowMs: 60 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 10 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
