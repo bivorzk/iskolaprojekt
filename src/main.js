79064a3553212159953db6bd8bcbc067747b8591
Htomi005 <hargitai.tamas.tech2021a@bolyaimovar.com>
2026-02-02 18:30:23 +0100

    Backend: add parent payment route

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ const admin = require('./admin/admin');
 const Order = require('./Orders/Order');
 const redisLuaService = require('./services/redis-lua-service');
 const cors = require('cors');
+const parentPayRouter = require('./payments/parent-pay');
 
 
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
@@ -174,6 +175,7 @@ app.use('/dashboard', dashboardRouter);
 app.use('/', logoutRouter);
 app.use('/admin', admin);
 app.use('/order', Order);
+app.use('/api/parent', parentPayRouter);
 
 
 // 404 handler
